e7d3c11c0dee3a6473626bb7fed652f9cc6e627d
ayrton <ayrton.o.neves@gmail.com>
2025-10-06 17:55:40 -0400

    :tada: Init :tada:

--- setup.py
+++ setup.py
@@ -7,14 +7,13 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="image_processing",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
-    description="My short description",
+    author="Ayrton",
+    description="Image Processing Package using Skimage",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/auuryon/image-processing-package",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
